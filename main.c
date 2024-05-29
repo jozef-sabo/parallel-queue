6860ce9051415742636c3027b7adc2b6de206bbe
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2024-05-29 22:03:22 +0200

    added calls to main

--- main.c
+++ main.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 #include <pthread.h>
-#include "types.h"
 
 #if defined(MONOTHREAD)
 #include "queue_monothread.h"
 #elif defined(LOCKFREE)
 #include "queue_lockfree.h"
 #elif defined(MUTEX)
-
+#include "queue_mutex.h"
 #endif
-
-
-#include "types.h"
 
 queue_t *queue_glob;
 
-void* vlakno(void *) {
+void push_elements(int number) {
     int test_int = 3;
-    int test_int_2 = 4;
-    int test_int_3 = 5;
-    int test_int_4 = 6;
-
-    for (int i = 0; i < 25000; ++i) {
+    for (int i = 0; i < number; ++i) {
         queue_push(queue_glob, &test_int);
-        queue_push(queue_glob, &test_int_2);
-        queue_push(queue_glob, &test_int_3);
-        queue_push(queue_glob, &test_int_4);
-        queue_push(queue_glob, &test_int_3);
-        queue_push(queue_glob, &test_int_2);
     }
-    printf("finished thread\n");
+}
 
-    pthread_exit(0);
+void* push_thread(void *handle) {
+    push_elements(30000);
+//    printf("finished thread\n");
+    return NULL;
+}
+
+void* pop_thread(void *handle) {
+
+    for (int i = 0; i < 50000; ++i) {
+        queue_pop(queue_glob, NULL);
+    }
+    return NULL;
 }
 
 int main(void) {
@@ -39,37 +37,73 @@ int main(void) {
 
 //    queue_t *queue;
     queue_init(&queue_glob);
-    printf("%d\n", queue_is_empty(queue_glob));
+//    printf("%d\n", queue_is_empty(queue_glob));
 
     void *r;
-    pthread_t p1,p2,p3, p4, p5, p6, p7,p8,p9, p10, p11, p12;
-    pthread_create(&p1,0,vlakno,0);
-    pthread_create(&p2,0,vlakno,0);
-//    pthread_create(&p3,0,vlakno,0);
-//    pthread_create(&p4,0,vlakno,0);
-//    pthread_create(&p5,0,vlakno,0);
-//    pthread_create(&p6,0,vlakno,0);
-//    pthread_create(&p7,0,vlakno,0);
-//    pthread_create(&p8,0,vlakno,0);
-//    pthread_create(&p9,0,vlakno,0);
-//    pthread_create(&p10,0,vlakno,0);
-//    pthread_create(&p11,0,vlakno,0);
-//    pthread_create(&p12,0,vlakno,0);
-    pthread_join(p1,&r);
-    pthread_join(p2,&r);
-//    pthread_join(p3,&r);
-//    pthread_join(p4,&r);
-//    pthread_join(p5,&r);
-//    pthread_join(p6,&r);
-//    pthread_join(p7,&r);
-//    pthread_join(p8,&r);
-//    pthread_join(p9,&r);
-//    pthread_join(p10,&r);
-//    pthread_join(p11,&r);
-//    pthread_join(p12,&r);
+    pthread_t p1_pop,p2_pop,p3_pop, p4_pop, p5_pop, p6_pop, p7_pop,p8_pop,p9_pop, p10_pop, p11_pop, p12_pop;
+    pthread_t p1_push,p2_push,p3_push, p4_push, p5_push, p6_push, p7_push,p8_push,p9_push, p10_push, p11_push, p12_push;
+    push_elements(150000);
 
-    printf("%d\n", queue_is_empty(queue_glob));
-    printf("Queue length: %llu\n", queue_size(queue_glob));
+    pthread_create(&p1_push, 0, push_thread, 0);
+    pthread_create(&p1_pop, 0, pop_thread, 0);
+
+    pthread_create(&p2_push, 0, push_thread, 0);
+    pthread_create(&p2_pop, 0, pop_thread, 0);
+
+
+    pthread_create(&p3_push, 0, push_thread, 0);
+    pthread_create(&p4_push, 0, push_thread, 0);
+    pthread_create(&p3_pop, 0, pop_thread, 0);
+    pthread_create(&p4_pop, 0, pop_thread, 0);
+
+    pthread_create(&p5_push, 0, push_thread, 0);
+    pthread_create(&p5_pop, 0, pop_thread, 0);
+    pthread_create(&p6_pop, 0, pop_thread, 0);
+    pthread_create(&p7_pop, 0, pop_thread, 0);
+    pthread_create(&p8_pop, 0, pop_thread, 0);
+
+    pthread_create(&p6_push, 0, push_thread, 0);
+    pthread_create(&p7_push, 0, push_thread, 0);
+    pthread_create(&p8_push, 0, push_thread, 0);
+    pthread_create(&p9_push, 0, push_thread, 0);
+    pthread_create(&p10_push, 0, push_thread, 0);
+    pthread_create(&p11_push, 0, push_thread, 0);
+    pthread_create(&p12_push, 0, push_thread, 0);
+    pthread_create(&p9_pop, 0, pop_thread, 0);
+    pthread_create(&p10_pop, 0, pop_thread, 0);
+    pthread_create(&p11_pop, 0, pop_thread, 0);
+    pthread_create(&p12_pop, 0, pop_thread, 0);
+
+    pthread_join(p1_pop, &r);
+    pthread_join(p2_pop, &r);
+    pthread_join(p3_pop, &r);
+    pthread_join(p4_pop, &r);
+    pthread_join(p5_pop, &r);
+    pthread_join(p6_pop, &r);
+    pthread_join(p7_pop, &r);
+    pthread_join(p8_pop, &r);
+    pthread_join(p9_pop, &r);
+    pthread_join(p10_pop, &r);
+    pthread_join(p11_pop, &r);
+    pthread_join(p12_pop, &r);
+
+    pthread_join(p1_push, &r);
+    pthread_join(p2_push, &r);
+    pthread_join(p3_push, &r);
+    pthread_join(p4_push, &r);
+    pthread_join(p5_push, &r);
+    pthread_join(p6_push, &r);
+    pthread_join(p7_push, &r);
+    pthread_join(p8_push, &r);
+    pthread_join(p9_push, &r);
+    pthread_join(p10_push, &r);
+    pthread_join(p11_push, &r);
+    pthread_join(p12_push, &r);
+
+
+
+//    printf("%d\n", queue_is_empty(queue_glob));
+//    printf("Queue length: %llu\n", queue_size(queue_glob));
 
 //    queue_print(queue);
 
